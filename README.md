0cac7efc3487289e07e52a101a8d8ad472bcf56d
luismedinacoca <luismedinacoca@gmail.com>
2020-09-27 21:51:53 -0300

    Section20 - lecture033 - Selenium Integration - Stage 2 of 2

---
+++ README.md
@@ -0,0 +1,12 @@
+1. Issues:
+    1.1 MainRunner:
+        "cucumber.runtime.CucumberException: Couldn't load plugin class: com.cucumber.listener.ExtentCucumberFormatter"
+2. Need to install from maven dependencies:
+    2.1 cucumber-core
+    2.2 cucumber-java
+    2.3 cucumber-java-deps
+    2.4 cucumber-jvm
+    2.5 Gherkin
+    2.6 Selenium
+3. 
+    
--- src/test/java/com/stackoverflow/steps/LoginSteps.java
+++ src/test/java/com/stackoverflow/steps/LoginSteps.java
@@ -1,50 +1,65 @@
 package com.stackoverflow.steps;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class LoginSteps {
 
+    WebDriver driver;
+
+    @Before()
+    public void setup(){
+        System.setProperty("webdriver.gecko.driver", "src\\test\\resources\\Webdrivers\\geckodriver.exe");
+        this.driver = new FirefoxDriver();
+        this.driver.manage().window().maximize();
+        this.driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+    }
+
     @Given("User navigates to stackoverflow website")
     public void user_navigates_to_stackoverflow_website() {
-        // Write code here that turns the phrase above into concrete actions
-        //throw new io.cucumber.java.PendingException();
-        System.out.println("Steps 01: User navigates to stackoverflow website");
+        driver.get("https://stackoverflow.com");
     }
 
     @Given("User clicks on the login button on homepage")
     public void user_clicks_on_the_login_button_on_homepage() {
-        // Write code here that turns the phrase above into concrete actions
-        //throw new io.cucumber.java.PendingException();
-        System.out.println("Steps 02: User clicks on the login button on homepage");
+        driver.findElement(By.xpath("//a[contains(text(), 'Log in')]")).click();
     }
 
     @Given("User enters a valid username")
-    public void user_enters_a_valid_username() {
-        // Write code here that turns the phrase above into concrete actions
-        //throw new io.cucumber.java.PendingException();
-        System.out.println("Step 03: User enters a valid username");
+    public void user_enters_a_valid_username() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.findElement(By.xpath(".//*[@id='email']")).sendKeys("luismedinacoca@gmail.com");
     }
 
     @Given("User enters a valid password")
     public void user_enters_a_valid_password() {
-        // Write code here that turns the phrase above into concrete actions
-        //throw new io.cucumber.java.PendingException();
-        System.out.println("Step 04: User enters a valid password");
+        driver.findElement(By.xpath(".//*[@id='password']")).sendKeys("Ensenada2008");
     }
 
     @When("User clicks on the login button")
     public void user_clicks_on_the_login_button() {
-        // Write code here that turns the phrase above into concrete actions
-        //throw new io.cucumber.java.PendingException();
-        System.out.println("Step 05: User clicks on the login button");
+        driver.findElement(By.xpath(".//*[@id='submit-button']")).click();
     }
 
     @Then("User should be taken to the successful login page")
-    public void user_should_be_taken_to_the_successful_login_page() {
-        // Write code here that turns the phrase above into concrete actions
-        //throw new io.cucumber.java.PendingException();
-        System.out.println("Step 06: User should be taken to the successful login page");
+    public void user_should_be_taken_to_the_successful_login_page() throws InterruptedException {
+        Thread.sleep(3000);
+        WebElement askQuestionButton = driver.findElement(By.xpath("//a[contains(text(), 'Ask Question')]"));
+        Assert.assertEquals(true, askQuestionButton.isDisplayed());
+    }
+
+    @After()
+    public void close(){
+        this.driver.quit();
     }
 }
